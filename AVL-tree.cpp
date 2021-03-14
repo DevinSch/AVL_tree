174d8090b47f1b52070ce3bf54b5895f0aebceee
DevinSch <devin.schafthuizen@uleth.ca>
2021-03-14 09:37:54 -0600

    Rebalance/remove functions.
    - Started remove node function, not currently working.
    - Added a rebalance function so it can be called in remove node when its finished.
    - Minor changes and documentation.

--- AVL-tree.cpp
+++ AVL-tree.cpp
@@ -1,10 +1,16 @@
+/**
+* @Authors: Cassandra Olfert, Devin Schafthuizen, Kate Ye
+* @Class - CPSC 3620: Data structures and algorithms
+* @Data Structure Performance and Evaluation - AVL tress
+* @Date: April 12,2021
+*/
 #include <iostream>
 #include <fstream>
 
 class Node {
    public:
-  Node (int k) : value(k) {}
-  int value;
+  Node (int k) : Key(k) {}
+  int Key;
   Node* left = NULL;
   Node* right = NULL;
   int height = 1;
@@ -17,7 +23,7 @@ int height(Node* n) {
 }
 
 int max(int a, int b) {
-  return (a > b) ? a : b; // returns the larger value
+  return (a > b) ? a : b; // returns the larger Key
 }
 
 Node* rightRotate(Node* n) {
@@ -44,38 +50,60 @@ int getBalanceFactor(Node* N) {
   return height(N->left) - height(N->right);
 }
 
-Node* addNode(Node* node, int value) {
-  // add the node at an empty spot, or continue searching down the tree
-  if (node == NULL) {
-    Node* n = new Node(value);
-    return (n);
-  }
-  if (value < node->value)
-    node->left = addNode(node->left, value);
-  else if (value > node->value)
-    node->right = addNode(node->right, value);
-  else
-    return node;
-
+Node* rebalnce(Node* node, int Key) {
   // Rebalnce the tree after adding the node
   node->height = 1 + max(height(node->left), height(node->right));
   int balanceFactor = getBalanceFactor(node);
   if (balanceFactor > 1) {
-    if (value < node->left->value) {                 // Right rotate
+    if (Key < node->left->Key) {                 // Right rotate
       return rightRotate(node);
-    } else if (value > node->left->value) {         // Left Right rotate
+    } else if (Key > node->left->Key) {         // Left Right rotate
       node->left = leftRotate(node->left);
       return rightRotate(node);
     }
   }
   if (balanceFactor < -1) {
-    if (value > node->right->value) {               // Left Rotate
+    if (Key > node->right->Key) {               // Left Rotate
       return leftRotate(node);
-    } else if (value < node->right->value) {        // Right Left Rotate
+    } else if (Key < node->right->Key) {        // Right Left Rotate
       node->right = rightRotate(node->right);
       return leftRotate(node);
     }
   }
+  return node;
+
+}
+
+Node* addNode(Node* node, int Key) {
+  // add the node at an empty spot, or continue searching down the tree
+  if (node == NULL) {
+    Node* n = new Node(Key);
+    return (n);
+  }
+  if (Key < node->Key)
+    node->left = addNode(node->left, Key);
+  else if (Key > node->Key)
+    node->right = addNode(node->right, Key);
+  else
+    return node;
+
+  node = rebalnce(node, Key);
+  return node;
+}
+
+
+// Work in progress, Not currently working
+Node* removeNode(Node* node, int Key) {
+  if (node == NULL)
+    return node;
+  if (Key < node->Key)
+    node->left = removeNode(node->left, Key);
+  else if (Key > node->Key)
+    node->right = removeNode(node->right, Key);
+  else {
+  }
+
+  node = rebalnce(node, Key);
   return node;
 }
 
@@ -89,7 +117,7 @@ void displayTree(Node *root, std::string indent, bool last) {
       std::cout << "L----";
       indent += "|  ";
     }
-    std::cout << root->value << std::endl;
+    std::cout << root->Key << std::endl;
     displayTree(root->left, indent, false);
     displayTree(root->right, indent, true);
   }
